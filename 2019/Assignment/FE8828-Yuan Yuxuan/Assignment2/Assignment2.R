00d205bd3eca17f276025d3a57d11fc7d055d3b5
Yang Ye <leafyoung@gmail.com>
2019-09-24 01:45:00 +0800

    20190924_lec13

--- 2019/Assignment/FE8828-Yuan Yuxuan/Assignment2/Assignment2.R
+++ 2019/Assignment/FE8828-Yuan Yuxuan/Assignment2/Assignment2.R
@@ -3,12 +3,12 @@
 #Step1
 make_choice <- function(N, split_number){
   input_list <- sample(1:N, N, replace = FALSE)
-  evaluation_group = input_list[0:split_number]
-  selection_group = input_list[(split_number+1):(length(input_list))]
-  max_score = 0
+  evaluation_group <- input_list[0:split_number]
+  selection_group <- input_list[(split_number+1):(length(input_list))]
+  max_score <- 0
   for(e in evaluation_group){
     if(e>max_score){
-      max_score = e
+      max_score <- e
     }
   }
   
@@ -18,3 +18,39 @@ make_choice <- function(N, split_number){
     }
   }
 }
+
+getProbability <- function(N, iteration, split_number){
+  nGetN <- 0
+  for(i in 0:iteration){
+    res <- make_choice(N, split_number)
+    if(!is.null(res)){
+      if(res == N){
+        nGetN <- nGetN+1
+      }
+    }
+  }
+  return(nGetN/N)
+}
+
+#Step2
+#What does the optimal result should get from? Maximum probability to get N or maximum the expected score?
+find_optimal <- function(N){
+  maxProb <- 0 
+  optima_split <- 0
+  iteration <- 100 #In fact, hundreds iteration is not enough because the result is not constant throughout the running
+  for(i in 1:round((N/2), 0)){
+    current_prob <- getProbability(N, iteration, i)
+    if(current_prob > maxProb){
+      maxProb <- current_prob
+      optima_split <- i
+    }
+  }
+  return(optima_split)
+}
+
+#Final Solution (Hint)
+result <- list() #key is N, value is the corresponding optimal solution
+for(i in 3:100){
+  result[[i]] <- find_optimal(i)
+}
+
